f796238b0d2de9b4e1832a09a8e51719fedc3f1d
lehtio <laura.l.lehtio@gmail.com>
2025-03-15 12:20:15 +0100

    korjatut linkit, vaihtosivu ja infoboxi

--- portfolio/src/components/InfoBox.jsx
+++ portfolio/src/components/InfoBox.jsx
@@ -1,4 +1,5 @@
 import "../styles/InfoBox.css";
+import { Link } from "react-router-dom"; // React Router käyttöön
 
 export default function InfoBox() {
   return (
@@ -6,9 +7,10 @@ export default function InfoBox() {
       <h1>Welcome to my portfolio</h1>
       <p>
         Hi, I'm Laura, third year information and software engineering student. I'm currently doing my 
-        exchange semester in TU Graz studying industrial management. 
+        exchange semester in TU Graz studying industrial management. If you are interested to know more about my studies abroad, I highly recommend you to 
+        check my <Link to="/exchange">Exchange</Link> site.
       </p>
-      <p>Here you can find information about my projects, skills, and experience!. Don't hesitate to ask if you have something in your mind</p>
+      <p>Here you can find information about my projects, skills, and experience! Don't hesitate to ask if you have something in your mind.</p>
     </div>
   );
 }
