537636fbebcc8241f94cde6a3daa843bdd495597
lehtio <laura.l.lehtio@gmail.com>
2025-03-20 15:39:26 +0100

    projektien sisältöjen päivitystä

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,22 +3,45 @@ import projectImage from "../images/project-image.png"; // Importoidaan kuva
 
 
 const projects = [
-  { title: "Project 1", image: projectImage },
-  { title: "Project 2", image: projectImage },
-  { title: "Project 3", image: projectImage },
-  { title: "Project 4", image: projectImage },
+  { name: "This portfolio website", 
+    image: projectImage,
+    description: "This is a portfolio website that I created using React. I used React Router to create the different pages of the website. I also used CSS to style the website. I created this website to showcase my projects and skills.",
+    link: "https://github.com/lehtio/portfolio ",
+    },
+
+    { name: "bibtext converter", 
+      image: projectImage,
+      description: "We did small project in the course where we created a bibtext converter. The point of this project was learning how to handle project management and how to work in a team. We used Scrum as our project management method.",
+      link: "https://github.com/lehtio/miniproject?tab=readme-ov-file"},
+
+    { name: "Web project",
+      image: projectImage,
+      description: "Tässä omaa kivaa puuhaa. Tarkoituksena on rakentaa nettisivusto, jonne voi koota erilaisia opintoja helpottavia materiaaleja kuten lukusuunnitelmapohjat ja kootut linkit eri opintokokonaisuuksien Quizletteihin.",
+      link: "https://github.com/lehtio/webproject"},
+  
+    { name: "Small weather app",
+      image: projectImage,
+      description: "I created a small weather app using React. I used the OpenWeatherMap API to get the weather data. I created this app to practice using APIs in React.",
+      link: "https://github.com/lehtio/projects"},
+  
+    { name: "Ohjelmointi 2 kurssin vakuutustenhallinnan sovellus",
+      image: projectImage,
+      description: "Ohjelmointi 2 kurssin lopputyö. Tarkoituksena oli luoda vakuutustenhallintasovellus, jossa voi lisätä, poistaa ja muokata vakuutuksia. Sovellus on tehty Javalla parityönä.",
+      link: "https://github.com/osterava/Sovellus-vakuutusten-hallintaan-ohj2"},
+ 
  
 ];
 
 export default function Projects() {
   return (
     <div className="main-card">
-      <span className="title">My Projects</span>
+      <h2 className="title">My Projects</h2>
       <div className="cards">
         {projects.map((project, index) => (
-          <div className="card" key={index}>
-            <span className="card-title">{project.title}</span>
-            <img src={project.image} alt={project.title} />
+          <div key={index} className="card">
+            <img src={project.image} alt={project.name} />
+            <div className="card-title">{project.name}</div>
+            <div className="card-description">{project.description}</div>
           </div>
         ))}
       </div>
