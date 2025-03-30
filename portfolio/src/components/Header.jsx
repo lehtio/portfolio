c5e5e81a9130a76ba75fe8e3e5ccaab2c51051d8
lehtio <laura.l.lehtio@gmail.com>
2025-03-30 01:52:45 +0100

    css styling

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,48 +1,21 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom"; // React Router käyttöön
- import "../styles/NewHeader.css"; 
-import profileAvatar from "../images/profile-avatar.webp";
+ import "../styles/Header.css"; 
+
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const checkHeaderSize = () => {
-      const header = document.querySelector(".header");
-      if (header) {
-        const headerHeight = header.getBoundingClientRect().height;
-        const screenHeight = window.innerHeight;
-
-        if (headerHeight > screenHeight * 0.30 || window.scrollY > 50) {
-          setIsScrolled(true);
-        } else {
-          setIsScrolled(false);
-        }
-      }
-    };
-
-    window.addEventListener("scroll", checkHeaderSize);
-    window.addEventListener("resize", checkHeaderSize);
-    window.addEventListener("load", checkHeaderSize);
-
-    checkHeaderSize();
-
-    return () => {
-      
-    };
-  }, []);
-
+ 
   return (
-    <header className={`header ${isScrolled ? "scrolled" : ""}`}>
+    <header className="header">
       <nav className="nav-bar">
-        <h1 className="site-title">Portfolio</h1>
+        
       
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/cv">CV</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/cv">RESUME</Link></li>
+          <li><Link to="/projects">PROJECTS</Link></li>
           
-          <li><Link to="/exchange">Exchange</Link></li>
+          <li><Link to="/exchange">BLOG</Link></li>
         </ul>
       </nav>
     </header>
